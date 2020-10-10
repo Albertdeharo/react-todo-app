05e6764c9a8fe8d8094a5a91ac6aed56c05d464e
Albert de Haro <Albert.deharo.murcia@gmail.com>
2020-10-10 14:18:00 +0200

    Updates

---
+++ precache-manifest.8176ad53d7d5aee50ad7f20aa6b9ae9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ec8ead78452bf72dd6cce60bbfcf61",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "37acc9f8753e9f88728f",
+    "url": "/react-todo-app/static/css/main.3fcd11d6.chunk.css"
+  },
+  {
+    "revision": "9a410c100d8ec8ca3c43",
+    "url": "/react-todo-app/static/js/2.87b0eaec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.87b0eaec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37acc9f8753e9f88728f",
+    "url": "/react-todo-app/static/js/main.b6512cd1.chunk.js"
+  },
+  {
+    "revision": "7f607c3e693deaba8e3a",
+    "url": "/react-todo-app/static/js/runtime-main.93dbfc51.js"
+  }
+]);
